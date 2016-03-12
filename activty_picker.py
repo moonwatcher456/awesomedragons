39fbff21f5a48eb0254583008ebb8b265a286e86
moonwatcher456 <kat.m.scott@outlook.com>
2016-03-11 17:21:32 -0800

    Create activty_picker.py

---
+++ activty_picker.py
@@ -0,0 +1,15 @@
+#This is my fist progam
+
+# import andom pick
+import random
+# list of all the things to randomize
+# list of all the things to randomize
+my_list=['unicorn', 'griffen', 'dragon']
+        
+#print my_list[2]
+#pick something
+picked_item=random.choice(my_list)
+
+# show user the selection
+
+print picked_item
